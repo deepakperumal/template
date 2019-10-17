35d905e3553af6df95d80b4f2ea62d1837b52a40
akshay <mehtaakshay22@gmail.com>
2019-10-17 17:12:26 +0530

    my first git commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,13 +1,30 @@
-$(document).on('click', '.navbar-toggler', function (e) {
-  $('.navbar').addClass('bg-nav1')
-  e.stopPropagation();
-  $('.navbar').toggleClass('bg-nav')
-  if ($('.navbar').hasClass('bg-nav')) {
-    $('.navbar').removeClass('bg-transparent')
+$(function () {
 
-  } else {
-    $('.navbar').addClass('bg-transparent')
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
 
-  }
+    console.log(scroll);
+    if (scroll > 600) {
+      $("#nav").addClass("changebg");
+      $('.navbar').removeClass('bg-transparent')
+    } else {
+      $("#nav").removeClass("changebg");
+    }
+  })
 
-})
+  $(document).on('click', '.navbar-toggler', function (e) {
+    $('.navbar').addClass('bg-nav1')
+    e.stopPropagation();
+    $('.navbar').toggleClass('bg-nav')
+    if ($('.navbar').hasClass('bg-nav')) {
+      $('.navbar').removeClass('bg-transparent')
+
+    } else {
+      $('.navbar').addClass('bg-transparent')
+
+    }
+
+  })
+
+
+});
