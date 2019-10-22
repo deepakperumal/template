bacefe787217a346735efa878a377fdaf19bca0f
akshay <mehtaakshay22@gmail.com>
2019-10-22 15:14:45 +0530

    first commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,98 +1,29 @@
 $(function () {
-  AOS.init();
-  $(window).scroll(function () {
-    var scroll = $(window).scrollTop();
-    if (scroll > 600) {
-      $("#nav").addClass("changebg");
-      $('.navbar').removeClass('bg-transparent')
-    } else {
-      $("#nav").removeClass("changebg");
+  AOS.init(), $(window).scroll(function () {
+    $(window).scrollTop() > 600 ? ($("#nav").addClass("changebg"), $(".navbar").removeClass("bg-transparent")) : $("#nav").removeClass("changebg")
+  }), $(document).on("click", ".navbar-toggler", function (a) {
+    $(".navbar").addClass("bg-nav1"), a.stopPropagation(), $(".navbar").toggleClass("bg-nav"), $(".navbar").hasClass("bg-nav") ? $(".navbar").removeClass("bg-transparent") : $(".navbar").addClass("bg-transparent")
+  }), $(".btn1").click(function (a) {
+    $(this).hasClass("monthly") && !$(this).hasClass("border-l") ? ($(".yearly").removeClass("yearly").addClass("monthly"), $(this).removeClass("monthly").addClass("yearly")) : ($(".monthly").removeClass("monthly").addClass("yearly"), $(this).removeClass("yearly").addClass("monthly"))
+  }), $(".ic").click(function (a) {
+    var s = $(this).attr("data-id");
+    $(".carousal-image-wrap").eq(s).siblings().removeClass("fadeInLeft fadeInRight");
+    var e = "";
+    if (s > $(this).parent().find(".i-active").index()) e = "fadeInRight";
+    else {
+      if (!(s < $(this).parent().find(".i-active").index())) return;
+      e = "fadeInLeft"
     }
-  })
-
-  $(document).on('click', '.navbar-toggler', function (e) {
-    $('.navbar').addClass('bg-nav1')
-    e.stopPropagation();
-    $('.navbar').toggleClass('bg-nav')
-    if ($('.navbar').hasClass('bg-nav')) {
-      $('.navbar').removeClass('bg-transparent')
-
-    } else {
-      $('.navbar').addClass('bg-transparent')
-
+    $(".carousal-image-wrap").eq(s).removeClass("active1").addClass("animated " + e + " "), $(".carousal-image-wrap").eq(s).siblings().addClass("active1").removeClass("animated"), $(this).addClass("i-active").siblings().removeClass("i-active")
+  }), $(".ic1").click(function (a) {
+    var s = $(this).attr("data-id");
+    $(".carousal-image-wrap").eq(s).siblings().removeClass("fadeInLeft fadeInRight");
+    var e = "";
+    if (s > $(this).parent().find(".i-active").index()) e = "fadeInRight";
+    else {
+      if (!(s < $(this).parent().find(".i-active").index())) return;
+      e = "fadeInLeft"
     }
-
+    $(".carousal-image-wrap1").eq(s).removeClass("active1").addClass("animated " + e + " "), $(".carousal-image-wrap1").eq(s).siblings().addClass("active1").removeClass("animated " + e + " "), $(this).addClass("i-active").siblings().removeClass("i-active")
   })
-
-  $('.btn1').click(function (e) {
-
-    if ($(this).hasClass('monthly') && !$(this).hasClass('border-l')) {
-
-      $('.yearly').removeClass('yearly').addClass('monthly')
-      $(this).removeClass('monthly').addClass('yearly')
-
-    } else {
-      $('.monthly').removeClass('monthly').addClass('yearly')
-      $(this).removeClass('yearly').addClass('monthly')
-    }
-
-  })
-
-  $('.ic').click(function (e) {
-
-    var index = $(this).attr('data-id')
-
-    $('.carousal-image-wrap').eq(index).siblings().removeClass('fadeInLeft fadeInRight')
-
-    var class1 = '';
-
-
-
-
-    if (index > $(this).parent().find('.i-active').index())
-
-      class1 = 'fadeInRight'
-
-    else
-
-      class1 = 'fadeInLeft'
-
-    $('.carousal-image-wrap').eq(index).removeClass('active1').addClass('animated ' + class1 + ' ')
-    $('.carousal-image-wrap').eq(index).siblings().addClass('active1').removeClass('animated')
-
-    $(this).addClass('i-active').siblings().removeClass('i-active')
-
-
-  })
-
-
-  $('.ic1').click(function (e) {
-
-    var index = $(this).attr('data-id')
-    $('.carousal-image-wrap').eq(index).siblings().removeClass('fadeInLeft fadeInRight')
-
-    var class1 = '';
-
-
-
-
-    if (index > $(this).parent().find('.i-active').index())
-
-      class1 = 'fadeInRight'
-
-    else
-
-      class1 = 'fadeInLeft'
-
-
-
-    $('.carousal-image-wrap1').eq(index).removeClass('active1').addClass('animated ' + class1 + ' ')
-    $('.carousal-image-wrap1').eq(index).siblings().addClass('active1').removeClass('animated ' + class1 + ' ')
-
-    $(this).addClass('i-active').siblings().removeClass('i-active')
-
-
-  })
-
-
 });
